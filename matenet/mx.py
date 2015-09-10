930d0037d79e6620d5957239f38c7820019b82a8
Jared Sanson <jared@jared.geek.nz>
2015-09-10 16:55:20 +1200

    Properly implemented the MX status packet format
    (to the best of my reverse engineering knowledge)

--- matenet/mx.py
+++ matenet/mx.py
@@ -6,40 +6,60 @@
 
 __author__ = 'Jared'
 
-from cstruct import struct
+from struct import Struct
 from value import Value
 from matenet import MateNET
 
-# Raw mate status packet
-_MXStatusStruct = struct('>B4sBBBHHH', (
-    'sop',              # Start-of-packet? has the 9th bit set to denote the start of the packet
-    'q',                # TODO: Unsure what this contains
-    'amp_hours',        # Daily accumulated amp-hours (0-255 AH)
-    'kilowatt_peak',    # Daily kilowatts-peak (hundredths: 0.00-2.55 kW)
-    'status',           # Status (Same as the MATE serial protocol)
-    'kilowatt_hours',   # Kilowatt-hours (tenths: 0.0-6553.5 kWh)
-    'v_bat',            # Battery/Out voltage (tenths: 0.0-6553.5 V)
-    'v_pv'              # PV/In voltage (tenths: 0.0-6553.5 V)
-))
+class MXStatusPacket(object):
+    fmt = Struct('>BbbbBBBBBHH')
 
-class MXStatusPacket(_MXStatusStruct):
     STATUS_SLEEPING = 0
     STATUS_FLOATING = 1
     STATUS_BULK = 2
     STATUS_ABSORB = 3
     STATUS_EQUALIZE = 4
 
+    def __init__(self):
+        self.amp_hours = None
+        self.kilowatt_hours = None
+        self.pv_current = None
+        self.bat_current = None
+        self.pv_voltage = None
+        self.bat_voltage = None
+        self.status = None
+        self.errors = None
+
     @classmethod
     def from_buffer(cls, data):
-        raw = super(MXStatusPacket, cls).from_buffer(data)
-        # Wrap members in Value objects to make it more human-readable
-        raw.amp_hours       = Value(raw.amp_hours,             units='Ah', resolution=0)
-        raw.kilowatt_peak   = Value(raw.kilowatt_peak / 100.0, units='kW', resolution=2)
-        raw.kilowatt_hours  = Value(raw.kilowatt_hours / 10.0, units='kW', resolution=1)
-        raw.v_bat           = Value(raw.v_bat / 10.0,          units='V',  resolution=1)
-        raw.v_pv            = Value(raw.v_pv / 10.0,           units='V',  resolution=1)
-        # TODO: What does the 'q' member contain?
-        return raw
+        values = cls.fmt.unpack(data)
+        status = MXStatusPacket()
+        # Gaahh their protocol is messed up
+        # The following was determined by poking values at the MATE unit...
+        raw_ah = ((values[0] & 0x70) >> 4) | values[4]
+        status.amp_hours = Value(raw_ah, units='Ah', resolution=0)
+        status.pv_current = Value((128 + values[1]) % 256, units='A', resolution=0)
+        status.bat_current = Value((128 + values[2]) % 256, units='A', resolution=0)
+        raw_kwh = (values[3] << 8) | values[8]  # whyyyy
+        status.kilowatt_hours = Value(raw_kwh / 10.0, units='kWh', resolution=1)
+        # TODO: values[5] - Kilowatts peak??
+        status.status = values[6]
+        status.errors = values[7]
+        status.bat_voltage = Value(values[9] / 10.0, units='V', resolution=1)
+        status.pv_voltage = Value(values[10] / 10.0, units='V', resolution=1)
+        return status
+
+    def __repr__(self):
+        return "<MXStatusPacket>"
+
+    def __str__(self):
+        fmt = \
+"""MXStatus:
+    PV:  {pv_voltage} {pv_current}
+    Bat: {bat_voltage} {bat_current}
+    Today: {kilowatt_hours} {amp_hours}
+"""
+        return fmt.format(**self.__dict__)
+
 
 class MateMX(MateNET):
     """
@@ -50,6 +70,12 @@ class MateMX(MateNET):
         Request a status packet from the controller
         :return: A MateStatusPacket
         """
+
         self._send('\x00\x04\x00\x01\x00\x00\x00')
         data = self._recv()
-        return MXStatusPacket.from_buffer(data)
+        return MXStatusPacket.from_buffer(data[1:])
+
+
+if __name__ == "__main__":
+    status = MXStatusPacket.from_buffer('\x85\x82\x85\x00\x69\x3f\x01\x00\x1d\x01\x0c\x02\x6a')
+    print status
